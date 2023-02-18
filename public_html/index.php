443ddee27287d4e252c4a489d8e004c243b80d04
Andrew Elliott <aelliott@onatlas.com>
2023-02-17 17:02:11 -0800

    update front-end app to use component for cards

--- public_html/index.php
+++ public_html/index.php
@@ -11,49 +11,93 @@ try {
 }
 ?>
 
-
-
 <html>
 <head>
-    <!--<script type="text/javascript" src="//vuejs.org/js/vue.js"></script>-->
-    <script type="text/javascript" src="//unpkg.com/vue"></script>
+    <title>Duga Tasks</title>
+    <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.js"></script>
+    <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue-icons.js"></script>
+    <style>
+        body {
+            background-image: url("https://cdn.sstatic.net/Sites/codereview/img/background-image.png?v=f93b5df604a8");
+        }
+    </style>
 </head>
 <body>
 <div id="msg"></div>
+<script type="text/x-template" id="task-card-template">
+    <b-card
+        border-variant="primary"
+        :header="task.name"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        align="center"
+        class="m-1"
+    >
+        task card
+        <b-card-text>
+            Last Ran: {{ formatDate(task.last) }}
+        </b-card-text>
+        <b-card-text>
+            Next Run: {{ formatDate(task.next) }}
+        </b-card-text>
+    </b-card>
+</script>
 <div id="app">
-    <h2>Tasks</h2>
-    <ul>
-        <li v-for="task in tasks">
-            {{ task.name }}
-        </li>
-    </ul>
+    <h2 class="m-2">Tasks</h2>
+    <b-card-group deck class="m-1">
+        <task-card
+            v-for="task in firstTasks"
+            :task="task"
+        >
+        </task-card>
+    </b-card-group>
+
+    <b-card-group deck class="m-1">
+        <task-card
+            v-for="task in laterTasks"
+            :task="task"
+        >
+        </task-card>
+    </b-card-group>
 </div>
 <script type="text/javascript">
     const URL = 'https://duga.zomis.net/tasks';
     const messageConstainer = document.getElementById('msg');
 
+    Vue.component('task-card', {
+        template: '#task-card-template',
+        props: {
+            task: {
+                type: Object
+            }
+        },
+        methods: {
+            formatDate: function(timestamp) {
+                console.log(`timestamp: ${timestamp}`);
+                const date = new Date(0);
+                date.setUTCSeconds(timestamp);
+                return date.toLocaleString(); //date.toISOString('en-GB');
+            }
+        }
+    });
     new Vue({
         el: '#app',
         data: {
-            tasks: {},
+            tasks: <?= $tasksJSON ?>,
             loading: false
+        },
+        computed: {
+            firstTasks: function () {
+                return Object.values(this.tasks).slice(0, 2);
+            },
+            laterTasks: function() {
+                return Object.values(this.tasks).slice(2);
+            }
         }
     });
-    async function loadData() {
-        try {
-            // const response = await fetch(URL);
-            const data = <?= $tasksJSON ?>;
-
-            const lastCommentsScan = data['Comments scanning'].last;
-            const lastCommentsScanDate = new Date(data['Comments scanning'].last);
-            messageConstainer.innerHTML = `last scan: ${lastCommentsScan} date: ${lastCommentsScanDate.toISOString('en-GB')}`;
-
-        } catch (e) {
-            messageConstainer.innerHTML = `error: ${e.message}`;
-        }
-    }
-
-    loadData();
 </script>
 </body>
 </html>
