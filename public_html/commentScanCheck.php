de34b37046d67dffc16109650f75f8e1f8081f8a
Andrew Elliott <aelliott@onatlas.com>
2023-08-18 10:27:02 -0700

    upload files from 000webhost

--- public_html/commentScanCheck.php
+++ public_html/commentScanCheck.php
@@ -1,24 +1,9 @@
 <?php
-/**
- * This scans the list of tasks and checks to see when the last time the Comments scanning was run
- * If the time delta between the last run and the current time is more than 15 minutes then return a 500 response code
- *
- * This script is scanned via betteruptime.com
- */
-const URL = 'https://duga.zomis.net/tasks';
-function getTasks() {
-    $sh = curl_init(URL);
-    curl_setopt($sh, CURLOPT_RETURNTRANSFER, true);
 
-    $tasksJSON = curl_exec($sh);
-    if (curl_errno($sh)) {
-        $error = curl_error($sh);
-        throw new Exception('curl error: ' . $error);
-    }
-    return json_decode($tasksJSON);
-}
+include '../include/getTasks.php';
+
 try {
-    $tasks = getTasks();
+    $tasks = json_decode(getTasks());
     $lastCommentsTimestamp = $tasks->{'Comments scanning'}->last;
     $lastComments = date('c', $lastCommentsTimestamp);
     $nextCommentsTimestamp = $tasks->{'Comments scanning'}->next;
@@ -27,7 +12,7 @@ try {
     $current = date('c', $currentTimestamp);
     $difference = $currentTimestamp - $lastCommentsTimestamp;
     //print 'tasks: '.var_export($tasks, true);
-    if ($difference > 900) {
+    if ($difference > 1000) {
         http_response_code(500);
     }
 
--- public_html/index.php
+++ public_html/index.php
@@ -1,103 +1,124 @@
 <?php
 
 include '../include/getTasks.php';
-
+$exception = '';
 try {
     $tasksJSON = getTasks();
-
 } catch (Exception $e) {
-    echo 'exception: ' . $e->getMessage();
+    $exception = $e->getMessage();
     $tasksJSON = '{}';
 }
 ?>
-
 <html>
-<head>
-    <title>Duga Tasks</title>
-    <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.css">
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.js"></script>
-    <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue-icons.js"></script>
-    <style>
-        body {
-            background-image: url("https://cdn.sstatic.net/Sites/codereview/img/background-image.png?v=f93b5df604a8");
-        }
-    </style>
-</head>
-<body>
-<div id="msg"></div>
-<script type="text/x-template" id="task-card-template">
-    <b-card
-        border-variant="primary"
-        :header="task.name"
-        header-bg-variant="primary"
-        header-text-variant="white"
-        align="center"
-        class="m-1"
-    >
-        task card
-        <b-card-text>
-            Last Ran: {{ formatDate(task.last) }}
-        </b-card-text>
-        <b-card-text>
-            Next Run: {{ formatDate(task.next) }}
-        </b-card-text>
-    </b-card>
-</script>
-<div id="app">
-    <h2 class="m-2">Tasks</h2>
-    <b-card-group deck class="m-1">
-        <task-card
-            v-for="task in firstTasks"
-            :task="task"
-        >
-        </task-card>
-    </b-card-group>
+    <head>
+        <title>Duga Tasks</title>
+        <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.css">
 
-    <b-card-group deck class="m-1">
-        <task-card
-            v-for="task in laterTasks"
-            :task="task"
-        >
-        </task-card>
-    </b-card-group>
-</div>
-<script type="text/javascript">
-    const URL = 'https://duga.zomis.net/tasks';
-    const messageConstainer = document.getElementById('msg');
+        <!--<script type="text/javascript" src="//vuejs.org/js/vue.js"></script>
+        <script type="text/javascript" src="//unpkg.com/vue"></script>-->
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+        <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue.js"></script>
+        <script type="text/javascript" src="//unpkg.com/bootstrap-vue@2.23.0/dist/bootstrap-vue-icons.js"></script>
+        <style>
+            body {
+                background-image: url("https://cdn.sstatic.net/Sites/codereview/img/background-image.png?v=f93b5df604a8");
+            }
+            }
+        </style>
+    </head>
+    <body>
+        <div id="msg"></div>
+        <script type="text/x-template" id="task-card-template">
+            <b-card
+                    border-variant="primary"
+                    :header="task.name"
+                    header-bg-variant="primary"
+                    header-text-variant="white"
+                    align="center"
+                    class="m-1"
 
-    Vue.component('task-card', {
-        template: '#task-card-template',
-        props: {
-            task: {
-                type: Object
+            >
+                <b-card-text v-if="task.last">
+                    Last Ran: {{ formatDate(task.last) }}
+                </b-card-text>
+                <b-card-text>
+                    Next Run: {{ formatDate(task.next) }}
+                </b-card-text>
+            </b-card>
+        </script>
+        <div id="app">
+            <h2 class="m-2">Tasks</h2>
+            <b-card-group deck class="m-1">
+                <task-card
+                        v-for="task in firstTasks"
+                        :task="task"
+                >
+                </task-card>
+            </b-card-group>
+
+            <b-card-group deck class="m-1">
+                <task-card
+                        v-for="task in laterTasks"
+                        :task="task"
+                >
+                </task-card>
+            </b-card-group>
+        </div>
+        <script type="text/javascript">
+            const URL = 'https://duga.zomis.net/tasks';
+            const messageConstainer = document.getElementById('msg');
+
+            Vue.component('task-card', {
+                template: '#task-card-template',
+                props: {
+                    task: {
+                        type: Object
+                    }
+                },
+                methods: {
+                    formatDate: function(timestamp) {
+                        console.log(`timestamp: ${timestamp}`);
+                        const date = new Date(0);
+                        date.setUTCSeconds(timestamp);
+                        return date.toLocaleString(); //date.toISOString('en-GB');
+                    }
+                }
+            });
+            new Vue({
+                el: '#app',
+                data: {
+                    tasks: <?= $tasksJSON ?>, //{},
+                    loading: false
+                },
+                computed: {
+                    firstTasks: function () {
+                        return Object.values(this.tasks).slice(0, 2);
+                    },
+                    laterTasks: function() {
+                        return Object.values(this.tasks).slice(2);
+                    }
+                }
+            });
+            async function loadData() {
+                try {
+                    // const response = await fetch(URL);
+                    const data = <?= $tasksJSON ?>;
+                    //this.tasks = data;
+
+                    const lastCommentsScan = data['Comments scanning'].last;
+                    const lastCommentsScanDate = new Date(data['Comments scanning'].last);
+                    //messageConstainer.innerHTML = `last scan: ${lastCommentsScan} date: ${lastCommentsScanDate.toISOString('en-GB')}`;
+
+                } catch (e) {
+                    messageConstainer.innerHTML = `error: ${e.message}`;
+                }
             }
-        },
-        methods: {
-            formatDate: function(timestamp) {
-                console.log(`timestamp: ${timestamp}`);
-                const date = new Date(0);
-                date.setUTCSeconds(timestamp);
-                return date.toLocaleString(); //date.toISOString('en-GB');
-            }
-        }
-    });
-    new Vue({
-        el: '#app',
-        data: {
-            tasks: <?= $tasksJSON ?>,
-            loading: false
-        },
-        computed: {
-            firstTasks: function () {
-                return Object.values(this.tasks).slice(0, 2);
-            },
-            laterTasks: function() {
-                return Object.values(this.tasks).slice(2);
-            }
-        }
-    });
-</script>
-</body>
+
+            //loadData();
+        </script>
+        <small style="display: none">
+            <?= $exception;?>
+        </small>
+    </body>
 </html>
